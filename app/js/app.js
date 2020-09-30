a1c55d093b830fb3eae5a1a5fb826b24af52919a
sanioooook <sahok6264@gmail.com>
2020-09-30 18:18:10 +0300

    completed the task

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // this function runs when the DOM is ready, i.e. when the document has been parsed
-
+  document.getElementsByClassName('menu-toggle')[0].children[0].addEventListener('change', openBar);
   // all js code should go below this line
 });
+
+function openBar() {
+  let content = document.getElementsByClassName('content')[0];
+  if (document.getElementsByClassName('menu-toggle')[0].children[0].checked) {
+    document.getElementsByClassName('nav-links')[0].style.transform = 'none';
+    content.style.opacity = .38;
+    content.style.background = '#333333';
+  }
+  else {
+    document.getElementsByClassName('nav-links')[0].style.transform = 'translate(0,-200%)';
+    content.style.opacity = 1;
+    content.style.background = 'none';
+  }
+}
