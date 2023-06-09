ee82b21199c5e7ffb67fdc2c1eefbab2e7e18b09
Alex Horban <sahok6264@gmail.com>
2023-06-09 23:36:32 +0300

    Update gulpfile.js and npm-gulp.yml
    - Update node-version to 18.x in npm-gulp.yml
    - Add Deploy step to npm-gulp.yml
    - Add distDir variable to gulpfile.js
    - Change paths for scripts, styles, and images in gulpfile.js
    - Add html task to gulpfile.js
    - Add cleandist and build tasks to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 const filesToWatch = 'html,htm,txt,json,md,woff2'; // List of files extensions for watching & hard reload (comma separated)
 const imagesToWatch = 'jpg,jpeg,png,webp,svg'; // List of images extensions for watching & compression (comma separated)
 const baseDir = 'app'; // Base directory path without «/» at the end
+const distDir = 'dist'; // Distribution directory path without «/» at the end
 const isOnline = true; // If «false» - Browsersync will work offline without internet connection
 
 const {src, dest, parallel, series, watch} = require('gulp');
@@ -27,7 +28,7 @@ function scripts() {
   return src(paths.scripts.src)
     .pipe(concat(paths.jsOutputName))
     .pipe(uglify())
-    .pipe(dest(paths.scripts.dest))
+    .pipe(dest(distDir + '/' + paths.scripts.dest))
     .pipe(browserSync.stream())
 }
 
@@ -37,19 +38,19 @@ function styles() {
     .pipe(concat(paths.cssOutputName))
     .pipe(autoprefixer({overrideBrowserslist: ['last 10 versions'], grid: true}))
     .pipe(cleanCSS({level: {1: {specialComments: 0}}}))
-    .pipe(dest(paths.styles.dest))
+    .pipe(dest(distDir + '/' + paths.styles.dest))
     .pipe(browserSync.stream())
 }
 
 async function images() {
   return src(paths.images.src)
-    .pipe(newer(paths.images.dest))
+    .pipe(newer(distDir + '/' + paths.images.dest))
     .pipe((await imagemin).default())
-    .pipe(dest(paths.images.dest))
+    .pipe(dest(distDir + '/' + paths.images.dest))
 }
 
 async function cleanimg() {
-  return await (await del).deleteAsync('' + paths.images.dest + '/**/*', {force: true})
+  return await (await del).deleteAsync('' + distDir + '/' + paths.images.dest + '/**/*', {force: true})
 }
 
 const paths = {
@@ -58,21 +59,27 @@ const paths = {
       // 'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
       baseDir + '/js/app.js' // app.js. Always at the end
     ],
-    dest: baseDir + '/js',
+    dest: 'js',
   },
 
   styles: {
     src: baseDir + '/' + "scss" + '/main.scss',
-    dest: baseDir + '/css',
+    dest: 'css',
   },
 
   images: {
-    src: baseDir + '/images/src/**/*',
-    dest: baseDir + '/images/dest',
+    src: baseDir + '/img/*',
+    dest: 'img',
   },
 
   cssOutputName: 'styles.min.css',
   jsOutputName: 'scripts.min.js',
+}
+
+function html() {
+  return src(baseDir + '/index.html')
+    .pipe(dest(distDir))
+    .pipe(browserSync.stream());
 }
 
 function startwatch() {
@@ -82,9 +89,15 @@ function startwatch() {
   watch([baseDir + '/js/**/*.js', '!' + paths.scripts.dest + '/*.min.js'], {usePolling: true}, scripts);
 }
 
+async function cleandist() {
+  return await (await del).deleteAsync(distDir + '/**/*', {force: true});
+}
+
 exports.browsersync = browsersync;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.images = images;
 exports.cleanimg = cleanimg;
+exports.cleandist = cleandist;
+exports.build = series(cleandist, parallel(html, images, styles, scripts));
 exports.default = parallel(images, styles, scripts, browsersync, startwatch);
